9b46833a856000bb965bdf9ffb9fa4d3afd90dac
siva.s <siva.s@globant.com>
2023-08-18 13:32:44 +0530

    update(camera): added camera info and changed default to max

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,43 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import type { ChangeEvent } from "react";
 import WebCam from "react-webcam";
 import "./App.css";
 import {
-  useMonitoring,
-  SentryOperation,
+  // useMonitoring,
+  // SentryOperation,
   SentryTransaction,
   SentryTag,
   SentrySpan,
 } from "./context";
-import type { ChangeEvent } from "react";
-type ResolutionValueUnions =
-  | "default"
-  | "1920x1080"
-  | "1280x720"
-  | "640x480"
-  | "3840x2160";
+import * as Sentry from "@sentry/react";
+
+type ResolutionValueUnions = "1920x1080" | "1280x720" | "640x480" | "3840x2160";
 type BitRateValueUnions =
-  | "default"
   | "8000000000"
   | "800000000"
   | "8000000"
   | "800000"
   | "8000"
   | "800";
-type FrameRateValueUnions = "default" | "15" | "24" | "30" | "60";
+type FrameRateValueUnions = "15" | "24" | "30" | "60";
 
 interface ISelectRecord<T = string> {
   label: string;
   value: T;
 }
+interface IRecordedVideoState {
+  url: string;
+  name: string;
+  resolution?: ResolutionValueUnions;
+  bitRate?: BitRateValueUnions;
+  frameRate?: FrameRateValueUnions;
+  size: string;
+  type: string;
+  videoHeight?: number;
+  videoWidth?: number;
+  duration?: number;
+}
 const RESOLUTIONS: Array<ISelectRecord<ResolutionValueUnions>> = [
-  {
-    label: "Default",
-    value: "default",
-  },
   {
     label: "4K Ultra HD (3840x2160)",
     value: "3840x2160",
@@ -52,10 +56,6 @@ const RESOLUTIONS: Array<ISelectRecord<ResolutionValueUnions>> = [
   },
 ];
 const BIT_RATES: Array<ISelectRecord<BitRateValueUnions>> = [
-  {
-    label: "Default bps",
-    value: "default",
-  },
   {
     label: "1 GB bps",
     value: "8000000000",
@@ -83,10 +83,6 @@ const BIT_RATES: Array<ISelectRecord<BitRateValueUnions>> = [
 ];
 const FRAME_RATES: Array<ISelectRecord<FrameRateValueUnions>> = [
   {
-    label: "Default FPS",
-    value: "default",
-  },
-  {
     label: "15 FPS",
     value: "15",
   },
@@ -103,18 +99,16 @@ const FRAME_RATES: Array<ISelectRecord<FrameRateValueUnions>> = [
     value: "60",
   },
 ];
-interface IRecordedVideoState {
-  url: string;
-  name: string;
-  resolution: string;
-  bitRate: string;
-  frameRate: string;
-  size: string;
-  type: string;
-  videoHeight?: number;
-  videoWidth?: number;
-  duration?: number;
-}
+/**
+ * MODE
+ * PORTRAIT | LANDSCAPE
+ */
+const isPortrait = true,
+  COMPRESSION_RATIO = 0.8,
+  IS_SAFARI = /^((?!chrome|android).)*safari/i.test(navigator.userAgent),
+  MIME_TYPE = IS_SAFARI ? "video/mp4;codecs=avc1" : "video/webm;codecs=vp8",
+  EXTENSION = IS_SAFARI ? ".mp4" : ".webm";
+
 function App() {
   const mediaRecordRef = useRef<MediaRecorder | null>(null);
   const webCamRef = useRef<WebCam>(null);
@@ -122,17 +116,19 @@ function App() {
   // const mediaStream = useRef<MediaStream | null>(null);
 
   const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [fetchingCameraInfo, setFetchingCameraInfo] = useState<boolean>(true);
 
   const [currentResolution, setCurrentResolution] =
-    useState<ResolutionValueUnions>("default");
-  const [currentBitRate, setCurrentBitRate] =
-    useState<BitRateValueUnions>("default");
+    useState<ResolutionValueUnions>();
+  const [currentBitRate, setCurrentBitRate] = useState<BitRateValueUnions>();
   const [currentFrameRate, setCurrentFrameRate] =
-    useState<FrameRateValueUnions>("default");
+    useState<FrameRateValueUnions>();
+  const [currentCameraCapability, setCurrentCameraCapability] =
+    useState<MediaTrackCapabilities | null>(null);
   // const [recordingStatus,setRecordingStatus]=useState<"idle"|"recording">("idle")
   const [recordedVideo, setRecordedVideo] = useState<IRecordedVideoState[]>([]);
 
-  const { measurePerformance } = useMonitoring();
+  // const { measurePerformance } = useMonitoring();
 
   const bytesToSize = (bytes: number) => {
     const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
@@ -142,47 +138,85 @@ function App() {
     return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i];
   };
 
-  // const getCameraPermission = async () => {
-  //   if (!("MediaRecorder" in window)) return;
-  //   try {
-  //     const [mediaWidth, mediaHeight] =
-  //       currentResolution === "default" ? [] : currentResolution.split("x");
-  //     const videoStream = await navigator.mediaDevices.getUserMedia({
-  //       audio: false,
-  //       video: {
-  //         width: Number(mediaWidth),
-  //         height: Number(mediaHeight),
-  //         frameRate:
-  //           currentFrameRate === "default"
-  //             ? undefined
-  //             : Number(currentFrameRate),
-  //       },
-  //     });
-  //     mediaStream.current = new MediaStream(videoStream.getVideoTracks());
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
+  const getCameraCapability = async () => {
+    try {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = devices.filter(
+        (device) => device.kind === "videoinput"
+      );
 
+      const device = videoDevices[0];
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { deviceId: device.deviceId, facingMode: "environment" },
+      });
+      const track = stream.getVideoTracks()[0];
+      const capabilities = track.getCapabilities();
+      setCurrentCameraCapability(capabilities);
+      stream.getTracks().forEach((track) => track.stop());
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    setFetchingCameraInfo(false);
+  };
+
+  useEffect(() => {
+    getCameraCapability();
+  }, []);
+
+  useEffect(() => {
+    if (currentCameraCapability) {
+      const {
+        frameRate,
+        width: { max: supportedMaxWidth } = {},
+        height: { max: supportedMinHeight } = {},
+      } = currentCameraCapability;
+      if (frameRate?.max) {
+        const matchedFrameRateIndex = FRAME_RATES.findIndex(
+          (e) => e.value === String(frameRate.max)
+        );
+        setCurrentFrameRate(FRAME_RATES[matchedFrameRateIndex]?.value);
+      }
+      if (supportedMaxWidth && supportedMinHeight) {
+        const matchedResIndex = RESOLUTIONS.findIndex((e) => {
+          const [w, h] = e.value.split("x"); //1920x1080
+          return (
+            supportedMaxWidth >= Number(w) && supportedMinHeight >= Number(h)
+          );
+        });
+
+        setCurrentResolution(RESOLUTIONS[matchedResIndex]?.value);
+      }
+    }
+  }, [currentCameraCapability]);
+
+  useEffect(() => {
+    if (currentResolution && currentFrameRate) {
+      const [w, h] = currentResolution.split("x");
+      const bitRate =
+        Number(w) * Number(h) * Number(currentFrameRate) * COMPRESSION_RATIO;
+      const matchedBitRateIndex = BIT_RATES.findIndex(
+        (e) => Number(e.value) <= bitRate
+      );
+      setCurrentBitRate(BIT_RATES[matchedBitRateIndex]?.value);
+    }
+  }, [currentFrameRate, currentResolution]);
   const startRecording = async () => {
-    const transaction = measurePerformance(
-      SentryTransaction.VIDEO_PROCESSING,
-      SentryOperation.VIDEO_CAPTURE
-    );
+    const transaction = Sentry.startTransaction({
+      name: SentryTransaction.VIDEO_PROCESSING,
+      // op: SentryOperation.VIDEO_CAPTURE,
+    });
     transaction.setTag(
       SentryTag.INSPECTION_ID,
       `Random-${Math.floor(Math.random() * 100)}`
     );
 
-    transaction.startSpan(SentrySpan.ASK_PERMISSION, null);
-    // await getCameraPermission();
-    transaction.finishSpan(SentrySpan.ASK_PERMISSION);
-
-    transaction.startSpan(SentrySpan.TAKE_VIDEO, null);
+    const videoTakingSpan = transaction.startChild({
+      op: SentrySpan.TAKE_VIDEO,
+    });
     setIsRecording(true);
     if (!webCamRef.current?.stream) return alert("Cannot record Now");
     const media = new MediaRecorder(webCamRef.current.stream, {
-      mimeType: "video/webm",
+      mimeType: MIME_TYPE,
       bitsPerSecond: Number(currentBitRate),
     });
     mediaRecordRef.current = media;
@@ -190,14 +224,16 @@ function App() {
       console.log("OnError", event);
     };
     mediaRecordRef.current.onstop = () => {
-      transaction.finishSpan(SentrySpan.TAKE_VIDEO);
+      videoTakingSpan.finish();
 
-      transaction.startSpan(SentrySpan.BLOB_MERGING, null);
+      const blobMergingSpan = transaction.startChild({
+        op: SentrySpan.BLOB_MERGING,
+      });
       const [chunk] = mediaChunks.current;
       const blob = new Blob(mediaChunks.current, { type: chunk.type });
       const url = URL.createObjectURL(blob);
       const currentRecordVideoInfo = {
-        name: `VideoRecord-${recordedVideo.length + 1}`,
+        name: `VideoRecord-${recordedVideo.length + 1}${EXTENSION}`,
         resolution: currentResolution,
         bitRate: currentBitRate,
         frameRate: currentFrameRate,
@@ -207,7 +243,19 @@ function App() {
       };
       setRecordedVideo((prevState) => [...prevState, currentRecordVideoInfo]);
       mediaChunks.current = [];
-      transaction.finishSpan(SentrySpan.BLOB_MERGING);
+      blobMergingSpan.setData(
+        "RESOLUTION",
+        currentRecordVideoInfo["resolution"]
+      );
+      blobMergingSpan.setData("BIT_RATE", currentRecordVideoInfo["bitRate"]);
+      blobMergingSpan.setData(
+        "FRAME_RATE",
+        currentRecordVideoInfo["frameRate"]
+      );
+      blobMergingSpan.setData("SIZE", currentRecordVideoInfo["size"]);
+      blobMergingSpan.setData("TYPE", currentRecordVideoInfo["type"]);
+      blobMergingSpan.finish();
+      transaction.finish();
     };
     mediaRecordRef.current.ondataavailable = (event) => {
       console.log("Recording done");
@@ -245,9 +293,29 @@ function App() {
   ) => {
     setCurrentFrameRate(event.target.value);
   };
-  const [mediaWidth, mediaHeight] =
-    currentResolution === "default" ? [] : currentResolution.split("x");
-
+  const [mediaWidth, mediaHeight] = currentResolution
+    ? currentResolution.split("x")
+    : [];
+  const { frameRate, height, width } = currentCameraCapability ?? {};
+  if (fetchingCameraInfo) {
+    return (
+      <div
+        style={{
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className="loader" />
+        <div>
+          Fetching Camera info <br />
+          Press "Allow" to get Camera Info
+        </div>
+      </div>
+    );
+  }
   return (
     <div
       style={{
@@ -288,38 +356,62 @@ function App() {
           <WebCam
             style={{ height: "60vh", backgroundColor: "greenyellow" }}
             videoConstraints={{
-              width: Number(mediaWidth),
-              height: Number(mediaHeight),
-              frameRate:
-                currentFrameRate === "default"
-                  ? undefined
-                  : Number(currentFrameRate),
+              width: isPortrait ? Number(mediaHeight) : Number(mediaWidth),
+              height: isPortrait ? Number(mediaWidth) : Number(mediaHeight),
+              facingMode: "environment",
+              frameRate: currentFrameRate
+                ? undefined
+                : Number(currentFrameRate),
             }}
             ref={webCamRef}
             audio={false}
           />
-          <div>
-            <select value={currentResolution} onChange={onResolutionChange}>
-              {RESOLUTIONS.map(({ label, value }) => (
-                <option key={`resolutions#${value}`} value={value}>
-                  {label}
-                </option>
-              ))}
-            </select>
-            <select value={currentBitRate} onChange={onBitRateChange}>
-              {BIT_RATES.map(({ label, value }) => (
-                <option key={`bit_rates#${value}`} value={value}>
-                  {label}
-                </option>
-              ))}
-            </select>
-            <select value={currentFrameRate} onChange={onFrameRateChange}>
-              {FRAME_RATES.map(({ label, value }) => (
-                <option key={`frame_rates#${value}`} value={value}>
-                  {label}
-                </option>
-              ))}
-            </select>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+              padding: "10px 0px",
+            }}
+          >
+            <div>
+              <span>Resolution: </span>
+              <select value={currentResolution} onChange={onResolutionChange}>
+                {RESOLUTIONS.map(({ label, value }) => (
+                  <option key={`resolutions#${value}`} value={value}>
+                    {label}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <span>FPS: </span>
+              <select value={currentFrameRate} onChange={onFrameRateChange}>
+                {FRAME_RATES.map(({ label, value }) => (
+                  <option
+                    disabled={Number(value) > Number(frameRate?.max)}
+                    key={`frame_rates#${value}`}
+                    value={value}
+                  >
+                    {label}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <span>BitRate: </span>
+              <select
+                disabled
+                value={currentBitRate}
+                onChange={onBitRateChange}
+              >
+                {BIT_RATES.map(({ label, value }) => (
+                  <option key={`bit_rates#${value}`} value={value}>
+                    {label}
+                  </option>
+                ))}
+              </select>
+            </div>
             <br />
             <button onClick={isRecording ? stopRecording : startRecording}>
               {isRecording ? "Stop Recording" : "Start Record"}
@@ -334,6 +426,22 @@ function App() {
             overflowY: "scroll",
           }}
         >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              backgroundColor: "grey",
+              padding: "8px",
+              margin: "8px 0px",
+            }}
+          >
+            Camera Info
+            <br />
+            <span>Max FPS : {frameRate?.max || "N/A"}</span>
+            <span>
+              Max Resolution(WxH) : {`${width?.max}x${height?.max}` || "N/A"}
+            </span>
+          </div>
           {recordedVideo.map((ele, index) => (
             <div
               key={`Video#${index}`}
@@ -347,22 +455,14 @@ function App() {
             >
               <video
                 onLoadedMetadata={(event) => {
-                  const { duration, videoHeight, videoWidth } =
-                    event.currentTarget;
+                  const { videoHeight, videoWidth } = event.currentTarget;
                   setRecordedVideo((prevState) => {
                     prevState[index] = {
                       ...prevState[index],
-                      duration,
                       videoHeight,
                       videoWidth,
                     };
                     return [...prevState];
-                  });
-
-                  console.log(index, {
-                    duration,
-                    videoHeight,
-                    videoWidth,
                   });
                 }}
                 style={{ width: "150px", aspectRatio: 16 / 9 }}
@@ -386,6 +486,9 @@ function App() {
                     </p>
                   ))}
               </div>
+              <a href={ele.url} download={ele.name}>
+                Download
+              </a>
             </div>
           ))}
         </div>
@@ -393,5 +496,6 @@ function App() {
     </div>
   );
 }
+const SentryApp = Sentry.withProfiler(App);
 
-export default App;
+export default SentryApp;
